03f7ae292928fc1ced9170675e29fc65def4b681
Punit Dave <punitdave000@gmail.com>
2025-09-22 11:02:13 +0530

    Delete PrintBoard.py

--- PrintBoard.py
+++
@@ -1,6 +0,0 @@
-def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
-    print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
-    print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
